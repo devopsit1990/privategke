c551e60e826c337c9e7a6758d01ecf672cc9cbd1
devopsit1990 <64223138+devopsit1990@users.noreply.github.com>
2024-03-29 10:08:57 +0530

    Update 1-provider.tf

--- 1-provider.tf
+++ 1-provider.tf
@@ -1,14 +1,14 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 provider "google" {
-  project = "devops-v4"
+  project = "kubernetes-411307"
   region  = "us-central1"
 }
 
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
   backend "gcs" {
-    bucket = "antonputra-tf-state-staging"
-    prefix = "terraform/state"
+    bucket = "gcs_12"
+    prefix = "terraform/gke_state"
   }
   required_providers {
     google = {
